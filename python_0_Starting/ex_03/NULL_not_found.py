3fa1aad88b55fcd2237c616ce80d34b583eb7bbc
Scardell <153379926+scardell@users.noreply.github.com>
2024-04-11 09:25:49 +0200

    modificacion

--- python_0_Starting/ex_03/NULL_not_found.py
+++ python_0_Starting/ex_03/NULL_not_found.py
@@ -10,36 +10,15 @@ def NULL_not_found(object: any) -> int:
         False: "Fake"
     }
 
-    flag = 0
     type_name = type_names.get(object, "Type not Found")
 
-    if type_name == "Type not Found":
-        flag = 1
-    if object in type_names:
-        print(f"{type_names[object]}: {object} {type(object)}")
-    elif math.isnan(object):
+    if type(object) is float and math.isnan(object):
         print(f"Cheese: {object} {type(object)}")
-    else:
-        print("Type not found")
-    if flag == 1:
-        return 1
-    return 0
-"""
-    if object is None:
-        print(f"Nothing: {object} {type(object)}")
     elif object == 0 and type(object) is int:
         print(f"Zero: {object} {type(object)}")
-    elif type(object) is float:
-        print(f"Cheese: {object} {type(object)}")
-    elif object == '':
-        print(f"Empty: {object} {type(object)}")
-    elif type(object) is str and type_name != "Type not Found":
-        print(f"{type(object)}: {object}")
-    elif not object and type(object) is bool:
-        print(f"Fake: {object} {type(object)}")
-    else:  
-        print("Type not Found")
-    if flag == 1:
+    elif type_name != "Type not Found":
+        print(f"{type_name}: {object} {type(object)}")
+    else:
+        print("Type not found")
         return 1
     return 0
-"""
