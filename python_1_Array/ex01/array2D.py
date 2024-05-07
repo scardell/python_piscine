3f09e63c8d8892556d0939dbb3936ec3e08ee9b7
Scardell <153379926+scardell@users.noreply.github.com>
2024-05-07 18:09:37 +0200

    ex01_v1

--- python_1_Array/ex01/array2D.py
+++ python_1_Array/ex01/array2D.py
@@ -1,4 +1,21 @@
 
+import numpy as np
+
+
 def slice_me(family: list, start: int, end: int) -> list:
-#your code here
-    pass
+    """
+    Aqui va la documentacion.
+    """
+    try:
+        if not isinstance(family, list) \
+                or not isinstance(start, int) or not isinstance(end, int):
+            raise AssertionError("input must be a list and two integers.")
+        if not all(len(item) == len(family[0]) for item in family):
+            raise AssertionError("Input list with different sizes.")
+        print(f"My shape is : {np.array(family).shape}")
+        print(f"My new shape is : {np.array(family)[start:end+1].shape}")
+        return np.array(family)[start:end].tolist()
+        
+    except AssertionError as error:
+        print("\33[31m", AssertionError.__name__+":", error, "\33[0m")
+        return ""
